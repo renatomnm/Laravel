47b395c46da793bfd16e50813bb05f9bb78b6ee3
unknown <andres@digitalhouse.com>
2018-08-28 21:33:41 -0300

    adicionando o nomecompleto do ator no model

--- app/Ator.php
+++ app/Ator.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ator extends Model
 {
-  protected $table = 'actor'
+  protected $table = 'actors';
+
+  public function getNomeCompleto(){
+    return $this->first_name . " " . $this->last_name
+  }
 }
